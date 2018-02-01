c1197f8c7304e3069a271edb023d2cbb411a3e3f
Joshua Higgins <xxhiggins723xx@gmail.com>
2018-01-31 18:31:17 -0700

    finished lesson

--- web/js/main.js
+++ web/js/main.js
@@ -18,3 +18,19 @@ document.getElementsByClassName('rightArrowBtn')[0].onclick = function() {
     }  
 }
 // end rightArrow
+
+// transitionSquare2
+var transitionSquare2 = document.getElementsByClassName('transitionSquare2')[0];
+
+document.getElementsByClassName('transitionSquare2Btn')[0].onclick = function() {
+    if(this.innerHTML === 'Grow')
+    {
+        this.innerHTML = 'Shrink';
+        transitionSquare2.classList.add('transitionSquare2-active');
+    }
+    else {
+        this.innerHTML = 'Grow';
+        transitionSquare2.classList.remove('transitionSquare2-active');
+    }
+}
+// end transitionSquare2
